eed7fe3067c0c40d2d8f2fa2d051c3dcab40c9b2
cc2RG <rbgalloway88@gmail.com>
2015-11-22 21:22:29 +0000

    adding the latest attempt

---
+++ warehouse_picker/warehouse_picker_runner.rb
@@ -0,0 +1,21 @@
+require_relative('./warehouse_hash_functions')
+
+ #standard
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+
+  found_item = find_by_bay("b5")
+  puts "item at b5 found is: #{found_item}"
+
+  found_item = find_by_name("nail filer")
+  puts "nail filer is found at : #{found_item}"
+
+  #found_items = find_multiple_by_bay("b6")
+  #puts "Products are: #{found_items}"
+
+  #found_items = find_multiple_by_name()
+  #puts "Products collected from: #{found_items}"
+
+
+  #advanced
+#---------------------------------------------------------------------------------------------------------------------------------------
